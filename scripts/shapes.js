2f3d7f1a6e4bb25b4399f4b27df3bc7ab72073a7
jmalan210 <>
2026-02-19 18:44:33 -0700

    slide-out library working, needs css

--- scripts/shapes.js
+++ scripts/shapes.js
@@ -6,3 +6,10 @@ loadHeaderFooter();
 const shapesList = new ShapesList(".shapes-container");
 shapesList.loadFromJSON("./data/shapes.json")
     
+const sidebar = document.querySelector(".library-sidebar");
+const tab = document.querySelector(".library-tab");
+
+tab.addEventListener("click", () => {
+    const isOpen = sidebar.classList.toggle('open');
+    tab.setAttribute("aria-expanded", isOpen);
+});

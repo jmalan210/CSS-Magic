c76d9ec34fab3910cd48874db5106acd3813ba80
jmalan210 <>
2026-02-21 17:18:10 -0700

    added titles to shape cards

--- scripts/ShapesList.mjs
+++ scripts/ShapesList.mjs
@@ -10,10 +10,16 @@ export default class ShapesList {
         const li = document.createElement('li');
         li.classList.add('shape-card');
 
+        const capitalize = name => name[0].toUpperCase() + name.slice(1);
+        const title = document.createElement('h4');
+        title.classList.add('shape-card-title');
+        title.innerText = capitalize(name);
+
         const shapeElement = document.createElement('div');
         shapeElement.classList.add('shape', name);
 
         Object.entries(cssProps).forEach(([prop, value]) => {
+           
             shapeElement.style[prop] = value;
         });
 
@@ -23,6 +29,7 @@ export default class ShapesList {
             .map(([prop, value ]) => `${prop}: ${value};`)
             .join("\n");
         
+        li.appendChild(title);
         li.appendChild(shapeElement);
         li.appendChild(codeElement);
         this.container.appendChild(li);
